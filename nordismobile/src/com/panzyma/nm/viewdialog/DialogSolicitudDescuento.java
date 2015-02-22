d01d87fe8f57dc75e34f47431d1a6dc0e351412f
obenavidez <osmar.benavidez@gmail.com>
2015-02-21 20:27:25 -0600

    subiendo vista DialogSolicitudDescuento

--- nordismobile/src/com/panzyma/nm/viewdialog/DialogSolicitudDescuento.java
+++ nordismobile/src/com/panzyma/nm/viewdialog/DialogSolicitudDescuento.java
@@ -1,11 +1,16 @@
 package com.panzyma.nm.viewdialog;
      
+import static com.panzyma.nm.controller.ControllerProtocol.C_DATA;
+import static com.panzyma.nm.controller.ControllerProtocol.LOAD_DATA_FROM_LOCALHOST;
+
 import java.util.ArrayList;
 
 import com.panzyma.nm.NMApp;
+import com.panzyma.nm.auxiliar.NMConfig.SolicitudDescuento;
 import com.panzyma.nm.serviceproxy.ReciboColector;
 import com.panzyma.nm.serviceproxy.ReciboDetFactura;
 import com.panzyma.nm.view.ViewRecibo;
+import com.panzyma.nm.view.adapter.InvokeBridge;
 import com.panzyma.nordismobile.R;
 
 import android.app.Dialog; 
@@ -14,6 +19,7 @@ import android.os.Message;
 import android.widget.ListView;
 import android.widget.TextView;
 
+@InvokeBridge(bridgeName = "BSolicitudDescuentoM")
 public class DialogSolicitudDescuento extends Dialog  implements Handler.Callback{
 
 	ViewRecibo parent;
@@ -24,14 +30,13 @@ public class DialogSolicitudDescuento extends Dialog  implements Handler.Callbac
 									ArrayList<ReciboDetFactura> _facturas,
 									ReciboColector recibo) 
     {    
-    	super(_view,android.R.style.Theme_Translucent_NoTitleBar_Fullscreen);    
-        
+    	super(_view,android.R.style.Theme_Translucent_NoTitleBar_Fullscreen);          
         try 
-        {    
-	        
+        {    	        
         	setContentView(R.layout.solicituddescuento);   
 			parent=_view;      	 
 	        NMApp.getController().setView(this);
+	        NMApp.getController().getInboxHandler().sendEmptyMessage(LOAD_DATA_FROM_LOCALHOST);
 	        if(_facturas!=null)
 	        	facturas=_facturas;
 	        
@@ -53,7 +58,20 @@ public class DialogSolicitudDescuento extends Dialog  implements Handler.Callbac
 
 	@Override
 	public boolean handleMessage(Message msg) {
-		// TODO Auto-generated method stub
+		 
+		switch (msg.what) 
+		{
+			case C_DATA: 
+				break;
+		}
 		return false;
 	} 
+	
+	private void establecerDatos(ArrayList<SolicitudDescuento> solicitudes)
+	{
+		for(SolicitudDescuento s:solicitudes)
+		{
+			//if()
+		}
+	}
 }
