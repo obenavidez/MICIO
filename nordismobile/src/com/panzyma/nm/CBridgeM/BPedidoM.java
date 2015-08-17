aacfec7aa1dae074a4093ed2f444a541e3e1d880
alexcj23 <josue.herrera@outlook.com>
2015-08-17 04:52:34 +0200

    nm-pedidos-20150816-02
    no permitir actualizar el estado de cuenta del cliente, sino se ha
    asignado un id de sucursal al pedido

--- nordismobile/src/com/panzyma/nm/CBridgeM/BPedidoM.java
+++ nordismobile/src/com/panzyma/nm/CBridgeM/BPedidoM.java
@@ -264,8 +264,10 @@ public class BPedidoM extends BBaseM {
 						 
 						Processor.notifyToView(NMApp.getController(),ControllerProtocol.NOTIFICATION_DIALOG2,
 								0,0,new String("Actualizando el estado de cuentas del Cliente"));
-			            //Volver a traer al cliente del servidor y actualizarlo en la memoria del dispositivo            
-			            Cliente cliente= BClienteM.actualizarCliente(NMApp.getContext(),SessionManager.getCredenciales(),obj.getObjSucursalID()); 
+			            //Volver a traer al cliente del servidor y actualizarlo en la memoria del dispositivo 
+						Cliente cliente = null;
+						if( obj.getObjSucursalID() != 0)
+							cliente = BClienteM.actualizarCliente(NMApp.getContext(),SessionManager.getCredenciales(),obj.getObjSucursalID());						
 			           
 			            //Notificar al Usuario el resultado del envio del Pedido.
 			            NMApp.getController().notifyOutboxHandlers(ControllerProtocol.ID_REQUEST_UPDATEITEM_FROMSERVER, 0, 0,obj);
