85da10328db4cdcf94422eaaf896ffee9378d808
jovan688 <jovan688@gmail.com>
2016-01-27 22:05:21 -0600

    Agregando La Opcion Imprimir Cobros
    Agregando La Opcion Imprimir Cobros

--- nordismobile/src/com/panzyma/nm/CBridgeM/BCobroM.java
+++ nordismobile/src/com/panzyma/nm/CBridgeM/BCobroM.java
@@ -3,24 +3,36 @@ package com.panzyma.nm.CBridgeM;
 import static com.panzyma.nm.controller.ControllerProtocol.ERROR;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import com.panzyma.nm.NMApp;
+import com.panzyma.nm.auxiliar.DateUtil;
 import com.panzyma.nm.auxiliar.ErrorMessage;
 import com.panzyma.nm.auxiliar.NMNetWork;
 import com.panzyma.nm.auxiliar.Processor;
+import com.panzyma.nm.auxiliar.Session;
 import com.panzyma.nm.auxiliar.SessionManager;
+import com.panzyma.nm.auxiliar.StringUtil;
+import com.panzyma.nm.bluetooth.BluetoothConnection;
+import com.panzyma.nm.controller.ControllerProtocol;
 import com.panzyma.nm.model.ModelCobro;
 import com.panzyma.nm.serviceproxy.CCobro;
 import com.panzyma.nm.serviceproxy.CFormaPago;
+import com.panzyma.nm.serviceproxy.CobroDetalle;
+import com.panzyma.nm.serviceproxy.ReciboColector;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Message;
+import android.util.Log;
 
 public class BCobroM extends BBaseM{
 
-	
+	protected String TAG= BCobroM.class.toString();
 	public BCobroM(){}
 	
 	public enum Accion {
-		COBROS_DEL_DIA(0), COBROS_DEL_SEMANA(1), COBROS_DEL_MES(2) , PAGOS_DEL_DIA(3), PAGOS_DE_SEMANA(4), PAGOS_DEL_MES(5) ;
+		COBROS_DEL_DIA(0), COBROS_DEL_SEMANA(1), COBROS_DEL_MES(2) , PAGOS_DEL_DIA(3), PAGOS_DE_SEMANA(4), PAGOS_DEL_MES(5) , IMPRIMIR(6);
 		int result;
 
 		Accion(int result) {
@@ -58,6 +70,9 @@ public class BCobroM extends BBaseM{
 			break;
 		case  PAGOS_DEL_MES : 
 			LoadPagos(false, false, true, 0, 0, Accion.PAGOS_DEL_MES);
+			break;
+		case IMPRIMIR: 
+				ImprimirCobro((CobroDetalle)msg.obj);
 			break;
 		}
 		return false;
@@ -130,4 +145,125 @@ public class BCobroM extends BBaseM{
 		}
 	}
 
+	
+	public  void ImprimirCobro(CobroDetalle detalle ) 
+	{   
+		try
+		{
+			 String recibo = "";              
+	         String msg = "";
+	         
+	         
+	         //Encabezado del reporte
+	         recibo += "T 7 0 0 2 Distribuidora Panzyma - DISPAN\r\n";        
+	         recibo += "T 7 0 0 36 " + msg + "\r\n";  
+	         recibo += "LINE 0 70 576 70 1 \r\n";
+	         recibo += "T 7 0 0 80 Fecha:\r\n";
+	         recibo += "T 7 0 115 80 " + DateUtil.idateToStr(DateUtil.getToday()) + "\r\n";
+	         recibo += "T 7 0 0 110 Colector:\r\n";
+	         
+	         SharedPreferences pref = getContext().getSharedPreferences("LoginUser", Context.MODE_PRIVATE);
+	 		 String name = pref.getString("nombre", "");
+	         recibo += "T 7 0 115 110 " + name + "\r\n";
+
+	         recibo += "T 7 0 0 150 Recibo\r\n";
+	         recibo += "T 7 0 110 150 Fecha\r\n";
+	         recibo += "T 7 0 220 150 Cliente\r\n";
+	         recibo += "RIGHT 576\r\n"; 
+	         recibo += "T 7 0 0 149 Total\r\n";
+	         recibo += "LEFT\r\n"; 
+	         recibo += "LINE 1 175 576 175 1\r\n";
+	         
+	         int y = 180;
+	         double total = 0;
+	         
+	         for (CCobro c : detalle.getCobros()) {
+				total+= c.getTotalRecibo();
+				//Número del recibo
+                String numero = c.getReferencia();
+                if (c.getReferencia().compareTo("") == 0) numero = c.getNumeroCentral();
+                
+                recibo += "T 7 0 0 " + y + " " + numero + "\r\n";
+                recibo += "T 7 0 110 " + y + " " + c.getFecha() + "\r\n";
+                
+                String nombreCliente = c.getNombreCliente();
+                if (nombreCliente.length() > 18) 
+                    nombreCliente = nombreCliente.substring(0, 18);
+                    
+                recibo += "T 7 0 220 " + y + " " + nombreCliente + "\r\n";
+                
+                recibo += "RIGHT 576\r\n"; 
+                recibo += "T 7 0 0 " + y + " " + StringUtil.formatReal(c.getTotalRecibo()) + "\r\n";
+                recibo += "LEFT\r\n"; 
+                
+                y += 30;
+			}
+	         
+	         recibo += "LINE 0 " + y + " 576 " + y + " 1\r\n";
+	         y += 5;
+	         recibo += "T 7 0 0 " + y + " " + detalle.getCobros().size() + " recibos pagados.\r\n";
+
+	         recibo += "RIGHT 576\r\n"; 
+	         total = StringUtil.round(total, 2);
+	         recibo += "T 7 0 0 " + y + " " + StringUtil.formatReal(total) + "\r\n";
+	         recibo += "LEFT\r\n"; 
+
+	         y += 50;
+	         
+	         //Encabezado de detalles de pago
+	         recibo += "T 7 0 0 " + y + " Forma de Pago\r\n";                        
+	         recibo += "T 7 0 180 " + y + " Moneda\r\n";
+	         recibo += "RIGHT 400\r\n"; 
+	         recibo += "T 7 0 0 " + y + " Monto\r\n";            
+	         recibo += "RIGHT 576\r\n"; 
+	         recibo += "T 7 0 0 " + y + " Monto Nacional\r\n";
+	         recibo += "LEFT\r\n"; 
+	         y += 30;
+	         recibo += "LINE 1 " + y + " 576 " + y + " 1\r\n";
+	         y += 5;
+
+	         //Detalle de pagos
+	         total = 0;
+	         for (CFormaPago p : detalle.getFormapagos()) {
+	        	 total += p.getMontoNacional();
+	                
+	                recibo += "T 7 0 0 " + y + " " + p.getDescFormaPago() + "\r\n";            
+	                recibo += "T 7 0 180 " + y + " " + p.getDescMoneda() + "\r\n";     
+	                       
+	                recibo += "RIGHT 400\r\n"; 
+	                recibo += "T 7 0 0 " + y + " " + StringUtil.formatReal(p.getMonto()) + "\r\n";
+	            
+	                recibo += "RIGHT 576\r\n"; 
+	                recibo += "T 7 0 0 " + y + " " + StringUtil.formatReal(p.getMontoNacional()) + "\r\n";
+	                recibo += "LEFT\r\n"; 
+	                y += 30;
+			}
+	         recibo += "LINE 1 " + y + " 576 " + y + " 1\r\n";
+	         recibo += "RIGHT 576\r\n"; 
+	         total = StringUtil.round(total, 2);
+	         recibo += "T 7 0 0 " + y + " " + StringUtil.formatReal(total) + "\r\n";
+	         recibo += "LEFT\r\n"; 
+
+	         recibo += "FORM\r\n";
+	         recibo += "PRINT\r\n";
+
+	         y += 50; 
+
+	         String header = "! 0 200 200 " + y + " 1\r\n";
+	         header += "LABEL\r\n";
+	         header += "CONTRAST 0\r\n";
+	         header += "TONE 0\r\n";
+	         header += "SPEED 3\r\n";
+	         header += "PAGE-WIDTH 600\r\n";
+	         header += "BAR-SENSE\r\n";
+
+	         recibo = header + recibo;    
+	         new BluetoothConnection(recibo); 
+		}
+		catch (Exception e) 
+		 { 
+			 NMApp.getController().notifyOutboxHandlers(ControllerProtocol.ERROR, 0, 0,ErrorMessage.newInstance("",e.getMessage(),(e.getCause()==null)?"":e.getCause().toString()));
+			Log.d(TAG,"ERROR al tratar de envia el recibo", e);
+		}
+	}
 }
