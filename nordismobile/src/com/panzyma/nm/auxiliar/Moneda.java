b7667ee0cb5ee7dc29f519c7ebdbea975839137c
alexcj23 <herrera.valdivia.josue@gmail.com>
2016-04-20 23:41:15 -0300

    Currency

---
+++ nordismobile/src/com/panzyma/nm/auxiliar/Moneda.java
@@ -0,0 +1,26 @@
+package com.panzyma.nm.auxiliar;
+
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+
+public class Moneda {
+
+	private static Locale locale = new Locale("es", "NI");
+	
+	private Moneda(){}
+
+	public static String getSimbol() {
+		return Currency.getInstance(locale).getSymbol();
+	}
+	
+	public static Locale getLocale() {
+		return locale;
+	}
+	
+	public static String toCurrency(double valor) {
+		return getSimbol()+" "+String.format(Moneda.getLocale(), "%,.2f", valor);
+	}
+	
+
+}
